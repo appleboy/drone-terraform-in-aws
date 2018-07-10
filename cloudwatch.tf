b3523401ac1c8a22be0f9cab7f14c263dc52260e
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-11 00:52:04 +0800

    feat: add cloud watch.

---
+++ cloudwatch.tf
@@ -0,0 +1,11 @@
+resource "aws_cloudwatch_log_group" "drone_server" {
+  name = "drone/server"
+}
+
+resource "aws_cloudwatch_log_group" "drone_agent" {
+  name = "drone/agent"
+}
+
+resource "aws_cloudwatch_log_group" "drone_db" {
+  name = "drone/db"
+}
--- ecs.tf
+++ ecs.tf
@@ -2,18 +2,6 @@ resource "aws_ecs_cluster" "drone" {
   name = "drone"
 }
 
-resource "aws_cloudwatch_log_group" "drone_server" {
-  name = "drone/server"
-}
-
-resource "aws_cloudwatch_log_group" "drone_agent" {
-  name = "drone/agent"
-}
-
-resource "aws_cloudwatch_log_group" "drone_db" {
-  name = "drone/db"
-}
-
 data "template_file" "task_definition" {
   template = "${file("${path.module}/task-definitions/service.json")}"
 
@@ -31,10 +19,8 @@ resource "aws_ecs_task_definition" "drone" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
 
-  task_role_arn      = "${aws_iam_role.ecs_task.name}"
-  execution_role_arn = "${aws_iam_role.ecs_task.name}"
-
-  # execution_role_arn = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  task_role_arn      = "${aws_iam_role.ecs_task.arn}"
+  execution_role_arn = "${aws_iam_role.ecs_task.arn}"
 
   cpu    = "512"
   memory = "4096"
@@ -54,29 +40,6 @@ resource "aws_ecs_service" "drone" {
     subnets          = ["${aws_subnet.drone_a.id}", "${aws_subnet.drone_c.id}"]
     assign_public_ip = true
   }
-
-  # iam_role        = "${aws_iam_role.foo.arn}"
-  # depends_on      = ["aws_iam_role_policy.foo"]
-
-
-  # ordered_placement_strategy {
-  #   type  = "binpack"
-  #   field = "cpu"
-  # }
-
-
-  # load_balancer {
-  #   target_group_arn = "${aws_lb_target_group.foo.arn}"
-  #   container_name   = "mongo"
-  #   container_port   = 8080
-  # }
-
-
-  # placement_constraints {
-  #   type       = "memberOf"
-  #   expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  # }
-
   load_balancer {
     target_group_arn = "${aws_alb_target_group.drone.id}"
     container_name   = "drone-server"
